df261f8e60b673b54c2e9d99e5f9bbfbfd817686
ABY001 <abiolafadipe88@gmail.com>
2023-07-06 11:47:17 +0100

    reset fix

--- src/components/Intersection.js
+++ src/components/Intersection.js
@@ -42,7 +42,8 @@ const Intersection = () => {
     // Start the traffic light cycle again
     const switchLights = () => {
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      timerIds.forEach((timerId) => clearTimeout(timerId));
+      const getValue = () => timerIds;
+      getValue().forEach((timerId) => clearTimeout(timerId));
       // Switch lights for Street A and Street B
       setStreetALight('green');
       setStreetBLight('red');
